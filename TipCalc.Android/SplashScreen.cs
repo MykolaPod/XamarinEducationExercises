a26817fc278f0df3755794edfcbdbb743b1657af
MykolaPo <npodoprygora@ego-cms.com>
2017-04-24 14:07:14 +0300

    initial

---
+++ TipCalc.Android/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content.PM;
+using MvvmCross.Droid.Views;
+
+namespace TipCalc.Android
+{
+    [Activity(Label = "TipCalc Android", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashScreen : MvxSplashScreenActivity
+    {
+        public SplashScreen(): base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
---
+++ TipCalc.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.IoC;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+    public class App:MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes().EndingWith("Service").AsInterfaces().RegisterAsLazySingleton();
+
+            RegisterAppStart<FirstViewModel>();
+        }
+    }
+}
